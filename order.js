3a3f0c96d9374f74260379318049487eb1d0aee8
Danush S <143702971+danush00000@users.noreply.github.com>
2024-09-21 16:30:48 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49374',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    }, 
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '96999',
+        paymentStatus: 'Paid',
+        shipping: 'Deliverd'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Deliverd'
+    },
+]
